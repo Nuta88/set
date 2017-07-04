2b81789ffa341d89ca01e8cdc9c6fc9df345831a
nuta88 <anna.vagner-88@mail.ru>
2017-07-05 00:33:58 +0300

    Added change.

--- test.js
+++ test.js
@@ -3,17 +3,15 @@ QUnit.test("add method", function(assert) {
 	var actual = new Set();
 	actual.add("love");
 	actual.add("cat");
-	assert.deepEqual(actual.getArr(), expected,
-			"Two arrays can be the same in value");
-}); 
+	assert.deepEqual(actual.getArr(), expected, "Two arrays can be the same in value");
+});
 
 QUnit.test("find method", function(assert) {
 	var expected = true;
 	var actual = new Set();
 	actual.add("love");
 	actual.add("cat");
-	assert.deepEqual(actual.find("love"), expected,
-			"The array contains a value");
+	assert.deepEqual(actual.find("love"), expected, "The array contains a value");
 });
 
 QUnit.test("method of get elements by index ", function(assert) {
@@ -22,24 +20,18 @@ QUnit.test("method of get elements by index ", function(assert) {
 	actual.add("love");
 	actual.add("silly");
 	actual.add("cat");
-	assert.deepEqual(actual.getElemByIndex(1), expected,
-			"The array contains an index");
+	assert.deepEqual(actual.getElemByIndex(1), expected, "The array contains an index");
 });
 
-QUnit
-		.test(
-				"method of delete elements",
-				function(assert) {
-					var expected = [ "silly", "cat" ];
-					actual = new Set();
-					actual.add("love");
-					actual.add("silly");
-					actual.add("cat");
-					actual.delElemFromAArr(0, 1)
-					assert
-							.deepEqual(actual.getArr(), expected,
-									"The value deleted from array. Two arrays can be the same in value");
-				});
+QUnit.test("method of delete elements", function(assert) {
+	var expected = [ "silly", "cat" ];
+	actual = new Set();
+	actual.add("love");
+	actual.add("silly");
+	actual.add("cat");
+	actual.delElemFromAArr(0, 1)
+	assert.deepEqual(actual.getArr(), expected, "The value deleted from array. Two arrays can be the same in value");
+});
 
 QUnit.test("sort method", function(assert) {
 	var expected = [ "amazing", "cat", "love", "silly" ];
@@ -49,8 +41,7 @@ QUnit.test("sort method", function(assert) {
 	actual.add("cat");
 	actual.add("amazing");
 	actual.sortArr();
-	assert.deepEqual(actual.getArr(), expected,
-			"Two arrays can be the same in value");
+	assert.deepEqual(actual.getArr(), expected, "Two arrays can be the same in value");
 });
 
 QUnit.test("method of obtaining an array", function(assert) {
@@ -60,6 +51,5 @@ QUnit.test("method of obtaining an array", function(assert) {
 	actual.add("silly");
 	actual.add("cat");
 	actual.add("amazing");
-	assert.deepEqual(actual.getArr(), expected,
-			"Two arrays can be the same in value");
+	assert.deepEqual(actual.getArr(), expected, "Two arrays can be the same in value");
 });
